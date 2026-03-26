b25af10f321515a13ed355adb6a68d5e7628bc96
myusercloud <lucasharrizon@gmail.com>
2026-03-26 19:28:09 +0300

    Clean build ready for deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/footer";
 import Hero from "../components/home/hero";
@@ -5,14 +6,28 @@ import Categories from "../components/home/Categories";
 import FeaturedProducts from "../components/home/FeaturedProducts";
 
 function Home() {
+  // Global Theme Colors for consistency
+  const beigeWhite = "#FAF9F6";
+
   return (
-    <>
+    <Box bg={beigeWhite} minH="100vh">
+      {/* Navigation - Sticky at the top */}
       <Navbar />
-      <Hero />
-      <Categories />
-      <FeaturedProducts />
+
+      <Box as="main">
+        {/* Impactful Entry */}
+        <Hero />
+
+        {/* Navigation by Intent */}
+        <Categories />
+
+        {/* Curated Selection */}
+        <FeaturedProducts />
+      </Box>
+
+      {/* Deep Black finish at the bottom */}
       <Footer />
-    </>
+    </Box>
   );
 }
 
